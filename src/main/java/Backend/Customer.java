0b5bb10df3ec6195af4239380c0bdf985af66e17
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-03-21 18:25:06 +0530

    updated Code for Rental Iterator

--- src/main/java/Backend/Customer.java
+++ src/main/java/Backend/Customer.java
@@ -4,12 +4,9 @@ public class Customer {
 
     private String customerId;
 
-    private String documents;
-
     public Customer(String customerName, String customerId) {
         this.customerName = customerName;
         this.customerId = customerId;
-        this.documents = documents;
     }
 
     public String getCustomerId() {
